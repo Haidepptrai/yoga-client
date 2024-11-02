53427c7c1230abb338657a6e3b17026a9aaa8a85
Haidepptrai <hainguyenhoangcontact@gmail.com>
2024-11-02 17:37:49 +0700

    feat: get yoga course with firebase

--- components/YogaCourseList.tsx
+++ components/YogaCourseList.tsx
@@ -1,38 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, FlatList, StyleSheet } from "react-native";
 import YogaCourseCard from "./YogaCourseCard";
 import { YogaCourse } from "@/interface/YogaCourse";
 import { ThemedText } from "./ThemedText";
-
-const yogaClasses: YogaCourse[] = [
-  {
-    id: 1,
-    dayOfWeek: "Monday",
-    timeOfCourse: "10:00 AM",
-    duration: 60,
-    capacity: 20,
-    pricePerClass: 15,
-    typeOfClass: "Hatha Yoga",
-    description: "Beginner-friendly Hatha Yoga class",
-    isPublished: true,
-    category: "Beginner",
-  },
-  {
-    id: 2,
-    dayOfWeek: "Wednesday",
-    timeOfCourse: "1:00 PM",
-    duration: 75,
-    capacity: 15,
-    pricePerClass: 20,
-    typeOfClass: "Vinyasa Yoga",
-    description: "Flow-based yoga session for intermediates",
-    isPublished: true,
-    category: "Intermediate",
-  },
-  // Additional courses...
-];
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
 
 export default function YogaClassList() {
+  const [yogaClasses, setYogaClasses] = useState<YogaCourse[]>([]);
+
+  const categories = ["Beginner", "Intermediate", "Advanced"];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const subCollectionRef = collection(db, "yoga_courses");
+        const querySnapshot = await getDocs(subCollectionRef);
+
+        const subCollectionData: YogaCourse[] = querySnapshot.docs.map(
+          (doc) => ({
+            ...(doc.data() as YogaCourse), // Cast doc.data() to YogaCourse
+            category: categories[Math.floor(Math.random() * categories.length)], // Random category
+          })
+        );
+
+        setYogaClasses(subCollectionData);
+      } catch (error) {
+        console.error("Error fetching yoga courses:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const colors = [
     "#FAD7A0",
     "#FAD1C8",
